f3ddf63b39b4192509f0bce0236a8f9c0f1a4f99
StolzCesaro <107175051+StolzCesaro@users.noreply.github.com>
2023-05-10 21:38:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,110 +1,90 @@
 import pandas as pd
 import tkinter as tk
 import paramiko
-
 import os
+# citirea fișierului Excel
 
-file_name = "task3.xlsx"
-
-current_dir = os.getcwd()
-
-file_path = os.path.join(file_name)
-
-print(file_path)
-
-df = pd.read_excel('task3.xlsx', usecols=["Name", "Topology", "Owner", "VM", "MPlane"])
-
-# Funcție pentru afișarea datelor în funcție de preferințe
-
-def ssh_connect(hostname):
-    # Set up SSH connection parameters
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    hostname = '172.19.144.134'
-    username = 'adam'
-    password = '0000'
-
-    # Connect to the SSH server
-    try:
-        ssh.connect(hostname, username=username, password=password)
-        print('Connected to SSH server')
-    except paramiko.ssh_exception.AuthenticationException:
-        print('Authentication failed')
-    except:
-        print('Error connecting to SSH server')
+if os.path.exists("task3.xlsx"):
+    if "VM2" in pd.read_excel("task3.xlsx", nrows=0).columns:
+        df = pd.read_excel("task3.xlsx", usecols=["Name", "Topology", "Owner", "VM", "VM2", "MPlane"])
+    else:
+        df = pd.read_excel("task3.xlsx", usecols=["Name", "Topology", "Owner", "VM", "MPlane"])
+else:
+    print("Fișierul task3.xlsx nu a fost găsit în directorul curent.")
 
 
-    ssh.close()
 
-def display_data():
-    preferinte = search_entry.get()
-    results = df.loc[df['Name'] == preferinte]
+
+# definirea funcției de căutare după nume
+def search_name():
+    # citirea numelui din widget-ul de căutare
+    name = search_entry.get()
+    # selectarea rândurilor care conțin numele dat
+    results = df.loc[df["Name"] == name]
+    # afișarea rezultatelor în widget-ul de afișare
     display_text.delete("1.0", tk.END)
     display_text.insert(tk.END, results.to_string(index=False))
-root = tk.Tk()
-search_label = tk.Label(root, text="Nume")
-search_entry = tk.Entry(root)
-search_button = tk.Button(root, text="Cautare", command=display_data)
-display_text = tk.Text(root)
-
-button = tk.Button(root, text="Connect SSH", command=ssh_connect)
-button.pack(side=tk.BOTTOM)
-search_label.pack(side=tk.TOP)
-search_entry.pack(side=tk.TOP)
-search_button.pack(side=tk.TOP)
-
-display_text.pack(side=tk.BOTTOM)
+    # adăugarea butoanelor pentru conectare prin SSH
+    for index, row in results.iterrows():
+        ips = row["VM"].split(",")
+        for ip in ips:
+            if is_valid_ip(ip):
+                ssh_button = tk.Button(root, text=f"Conectare la {ip}", command=lambda ip=ip: connect_ssh(ip))
+                ssh_button.pack()
 
 
-root.mainloop()
+
+# funcție pentru a verifica dacă o adresă IP este validă
+def is_valid_ip(ip):
+    try:
+        parts = ip.split(".")
+        if len(parts) != 4:
+            return False
+        for part in parts:
+            if not 0 <= int(part) <= 255:
+                return False
+        return True
+    except:
+        return False
 
 
-def ssh_connect():
-    # Set up SSH connection parameters
+def execute_command(ssh, command):
+    # verificarea dacă obiectul transport este valabil
+    if ssh.get_transport() is not None:
+        # obținerea canalului SSH
+        channel = ssh.get_transport().open_session()
+        # trimiterea comenzii pe canal
+        channel.exec_command(command)
+        # citirea ieșirii comenzii
+        comanda = channel.recv(1024).decode().strip()
+        # afișarea ieșirii comenzii în consolă
+        display_text.insert(tk.END, f"\nNumele gazdei: {comanda}\n")
+
+
+# funcție pentru a efectua conexiunea SSH la o adresă IP dată
+def connect_ssh(ip):
+    # citirea numelui de utilizator și a parolei
+    username = username_entry.get()
+    password = password_entry.get()
+    # stabilirea conexiunii SSH
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    hostname = '172.19.144.134'
-    username = 'adam'
-    password = '0000'
+    ssh.connect(ip, username='adam', password='0000')
+    # afișarea output-ului comenzii "hostname"
+    stdin, stdout, stderr = ssh.exec_command("hostname")
+    output = stdout.read().decode().strip()
+    display_text.insert(tk.END, f"\nConectat la {ip} cu succes!\nNumele gazdei: {output}\n")
+    # adăugarea butoanelor pentru comenzi
+    command_button1 = tk.Button(root, text="Comanda 1", command=lambda: execute_command(ssh, "ls"))
+    command_button2 = tk.Button(root, text="Comanda 2")
+    command_button1.pack()
+    command_button2.pack()
 
+    # găsirea tuturor mașinilor virtuale aferente acestui SBTS
+    vms = []
+    for index, row in df.iterrows():
+        if row["Topology"] == ip:
+            vms.append(row["VM"])
+    # dacă există două mașini virtuale, adă
 
-    try:
-        ssh.connect(hostname, username=username, password=password)
-        print('Connected to SSH server')
-    except paramiko.ssh_exception.AuthenticationException:
-        print('Authentication failed')
-    except:
-        print('Error connecting to SSH server')
-
-
-    ssh.close()
-
-
-def check_ssh_connection():
-
-    ssh_connected = False  # Setăm la True dacă conexiunea SSH este stabilită
-
-
-    if ssh_connected:
-        button_ute_ca.config(state=tk.NORMAL)
-        button_agent_gve_common.config(state=tk.NORMAL)
-    else:
-        button_ute_ca.config(state=tk.DISABLED)
-        button_agent_gve_common.config(state=tk.DISABLED)
-
-    root = tk.Tk()
-    root.title("Interfață cu butoane")
-
-    # Creăm butonul UTE_CA
-    button_ute_ca = tk.Button(root, text="UTE_CA", state=tk.DISABLED)
-    button_ute_ca.pack()
-
-    # Creăm butonul AGENT_GVE_COMMON
-    button_agent_gve_common = tk.Button(root, text="AGENT_GVE_COMMON", state=tk.DISABLED)
-    button_agent_gve_common.pack()
-
-
-    check_ssh_connection()
-
-    root.mainloop()
 
